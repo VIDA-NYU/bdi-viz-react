6df903945f173d9cd294c369d8d7d9d9c7ac3ac7
Guande Wu <guandewu@nyu.edu>
2025-01-23 21:53:54 -0500

    fix the issues in merging

--- app/dashboard/components/embed-heatmap/types.ts
+++ app/dashboard/components/embed-heatmap/types.ts
@@ -1,6 +1,7 @@
 import { RectCell } from './cells/RectCell';
 import { BarCell } from './cells/BarCell';
 import { CellData } from './cells/types';
+import { ExpandedCellType } from './expanded-cells/types';
 
 type CellType = 'rect' | 'bar';
 type ColorScheme = 'blues' | 'viridis' | 'rdbu' | 'yellowBlue' | 'spectral' | 'greens' | 'oranges' | 'purples' | 'reds';
@@ -19,6 +20,7 @@ interface StackedHeatMapConfig {
     colorScalePadding: number;  // Percentage padding for color scale
     minScore: number;  // Optional manual override for min score
     maxScore: number;  // Optional manual override for max score
+    expandedCellType: ExpandedCellType;
 }
 
 export type {
